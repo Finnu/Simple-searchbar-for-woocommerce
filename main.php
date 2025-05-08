97db56fcf068cc2a00fa99862a964d30b614c450
Finnu <marqufin@gmail.com>
2025-05-08 11:05:19 +0200

    Main file

---
+++ main.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: SAS
+Plugin URI: https://github.com/Finnu/Simple-searchbar-for-woocommerce
+Description: Simple ajax search for woocommerce.
+Version: 1.0
+Author: Finnu
+Author URI: https://github.com/Finnu
+License: A "Slug" license name e.g. GPL2
+*/
+
+class CodeFinnSAS{
+    public static function search_products($queryString){
+
+        //Limit -1 to get count of all products with specified name
+        $params = array(
+          'search' => $queryString,
+          'status'  => 'publish',
+          'limit'   => -1,
+        );
+
+        $query = new WC_Product_Query($params);
+
+        $products = $query->get_products();
+
+        $amount = $query->get_total();
+
+        $result = array();
+
+        foreach ($products as $product) {
+
+            $current  = array(
+                'name' => $product->get_name(),
+                'price' => $product->get_price(),
+                'image' => $product->get_image(),
+                'url' => $product->get_permalink()
+            );
+
+            $result[] = $current;
+        }
+
+        return array("products" => $result, "amount" => $amount);
+
+    }
+}
+
+
+
